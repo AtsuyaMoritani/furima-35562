829dce534fcf6763989318bfda72ffc886a95163
AtsuyaMoritani <sengudunye@gmail.com>
2021-05-26 21:23:28 +0900

    コードの修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,19 @@
 class ItemsController < ApplicationController
-  before_action :set_prototype, except: [:index, :new, :create]
+  #before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index]
   def index
     
   end
   def new
     @item = Item.new
-     end
+  end
   def create
     @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
+   if @item.save
+    redirect_to root_path
+   else
+    render :new
+   end
   end
 end
 
@@ -21,7 +22,6 @@ private
 def item_params
    params.require(:item).permit(:product_name, :category_id, :price, :product_description, :product_condition_id, :ship_base_id, :region_id, :ship_date_id, :image).merge(user_id: current_user.id)
 end
-def set_prototype
-  @item = item.find(params[:id])
-end
-end
+ #def set_prototype
+ #  @item = item.find(params[:id])
+ #end
