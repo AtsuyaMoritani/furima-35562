e8846a714f32ffe7aafd0cb287de8fcea3a0c1b4
AtsuyaMoritani <sengudunye@gmail.com>
2021-06-03 20:27:39 +0900

    コードの修正3

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,5 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
 
 
--- db/migrate/20210531115039_create_buyer_addresses.rb
+++ db/migrate/20210531115039_create_buyer_addresses.rb
@@ -1,8 +1,8 @@
 class CreateBuyerAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :buyer_addresses do |t|
-      t.string     :postal_code,   null: false
-      t.integer    :region_id,     null: false
+      t.string     :postal_code
+      t.integer    :region_id
       t.string     :city
       t.string     :address
       t.string     :building_name
