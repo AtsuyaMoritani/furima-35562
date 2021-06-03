b3da57097673ab2a7cb8e16d35bc187df8bfe1a4
AtsuyaMoritani <sengudunye@gmail.com>
2021-06-03 17:06:20 +0900

    商品購入機能

---
+++ app/models/item_address.rb
@@ -0,0 +1,18 @@
+class ItemAddress
+  include ActiveModel::Model
+  attr_accessor  :buyer, :postal_code, :region_id, :city, :address, :building_name, :phone_number, :token, :item_id, :user_id
+
+  with_options presence: true do
+    validates :city
+    validates :address
+    validates :phone_number, format: /\A\d{10,11}\z/
+    validates :token
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :region_id, numericality: { other_than: 1 }
+  end
+
+  def save
+    buyer = Buyer.create(item_id: item_id, user_id: user_id)
+    BuyerAddress.create(postal_code: postal_code, region_id: region_id, city: city, address: address, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
+  end
+end
---
+++ spec/models/item_address_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe ItemAddress, type: :model do
+  describe '購入者情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @item_address = FactoryBot.build(:item_address, item_id: item.id, user_id: user.id)
+      sleep 0.1
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@item_address).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @item_address.building_name = ''
+        expect(@item_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @item_address.postal_code = ''
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @item_address.postal_code = '1231234'
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it 'region_idを選択していないと保存できないこと' do
+        @item_address.region_id = 1
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("Region must be other than 1")
+      end
+      it 'cityが空だと保存できないこと' do
+        @item_address.city = ''
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'addressが空だと保存できないこと' do
+        @item_address.address = ''
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @item_address.phone_number = ''
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'tokenが空だと保存できないこと' do
+        @item_address.token = ''
+        @item_address.valid?
+        expect(@item_address.errors.full_messages).to include("Token can't be blank")
+      end
+        it 'phone_numberが10字または11字以外だと保存できないこと' do
+          @item_address.phone_number = '0801111'
+          @item_address.valid?
+          expect(@item_address.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+end
+end
